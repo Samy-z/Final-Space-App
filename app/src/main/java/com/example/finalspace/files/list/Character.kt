d66f158763d1979a4c5cfbafc453b6dff4987d72
Samy-z <83598052+Samy-z@users.noreply.github.com>
2021-05-30 23:24:35 +0200

    update data class (2 data en plus)

--- app/src/main/java/com/example/finalspace/files/list/Character.kt
+++ app/src/main/java/com/example/finalspace/files/list/Character.kt
@@ -3,9 +3,10 @@ package com.example.finalspace.files.list
 data class Character(
         val id: Int,
         val name: String,
-    val species: String ,
-    val gender: String,
-    val hair: String,
-    val origin: String,
-    val img_url: String
+        val status: String,
+        val species: String,
+        val gender: String,
+        val hair: String,
+        val origin: String,
+        val img_url: String
 )
